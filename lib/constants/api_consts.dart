8fc5827e765392b7c8de96b90a2871408e028036
martin-ngigi <martinwainaina001@gmail.com>
2023-02-10 12:55:56 +0300

    Start displaying all the models

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,2 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "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";
--- lib/widgets/drop_down.dart
+++ lib/widgets/drop_down.dart
@@ -1,7 +1,11 @@
 //stful
 import 'package:chat_gpt/constants/constants.dart';
+import 'package:chat_gpt/models/models_model.dart';
+import 'package:chat_gpt/widgets/text_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../services/api_service.dart';
 
 class ModelsDropDownWidget extends StatefulWidget {
   const ModelsDropDownWidget({Key? key}) : super(key: key);
@@ -12,20 +16,39 @@ class ModelsDropDownWidget extends StatefulWidget {
 
 class _ModelsDropDownWidgetState extends State<ModelsDropDownWidget> {
 
-  String currentModel = "Model1";
+  String currentModel = "text-davinci-001";
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton(
-        dropdownColor: scaffoldBackgroundColor,
-        iconEnabledColor: Colors.white,
-        items: getModelsItem,
-        value: currentModel,
-        onChanged: (value){
-          setState(() {
-            currentModel = value.toString();
-          });
-        }
-    );
+    return FutureBuilder<List<ModelsModel>>(
+        future: ApiService.getModels(),
+        builder: (context, snapshot){
+          if(snapshot.hasError){
+            return Center(child: TextWidget(label: snapshot.error.toString()),);
+          }
+          return snapshot.data == null || snapshot.data!.isEmpty ? const SizedBox.shrink():
+          FittedBox(
+            child: DropdownButton(
+                dropdownColor: scaffoldBackgroundColor,
+                iconEnabledColor: Colors.white,
+                items:   List<DropdownMenuItem<String>>.generate(
+                    snapshot.data!.length,
+                        (index) => DropdownMenuItem(
+                        value:  snapshot.data![index].id,
+                        child: TextWidget(
+                          label:  snapshot.data![index].id,
+                          fontSize: 15,
+                        ))),
+                value: currentModel,
+                onChanged: (value){
+                  setState(() {
+                    currentModel = value.toString();
+                  });
+                }
+            ),
+          );
+        });
   }
+
+
 }
